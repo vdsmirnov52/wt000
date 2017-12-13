28c7c039abff610349b4b104a7ccec8d9cc70d38
Vadim Smirnov <vdsmitnov52@gmail.com>
2017-12-13 13:59:19 +0300

    smirnov@wdb CGI VDS
    	modified:   lib/dsqlite.py

--- lib/dsqlite.py
+++ lib/dsqlite.py
@@ -8,11 +8,11 @@ class dsqlite:
 	r""" Работа с SQLite
 	Warning('You can only execute one statement at a time.',) ... только одно заявление за раз
 Функции:
-	execute (query)		- Исполняет SQL запрос.	Возвращает: {True|False}
-	get_row  (query)	- Читает одну запись.   Возвращает: row = (val1, val2, ...)
-	get_rows (query)	- Читает несколько записей      Возвращает: rows = [(row1), (row2), ...]
-	get (query, fall)       - Исполняет запрос и читает данные. Если fall: 1 - fetchall() иначе 0 - fetchone())
-	get_table (tname, [swhere], [cols])
+	execute (query, [vals])		- Исполняет SQL запрос.	Возвращает: {True|False}
+	get_row  (query, [vals])	- Читает одну запись.   Возвращает: row = (val1, val2, ...)
+	get_rows (query, [vals])	- Читает несколько записей      Возвращает: rows = [(row1), (row2), ...]
+	get (query, fall, [vals])       - Исполняет запрос и читает данные. Если fall: 1 - fetchall() иначе 0 - fetchone())
+	get_table (tname, [swhere], [cols])	- Возвращает (desc, rows) или None
 Переменные:
 	desc = []	- Список наименования полей последнего запроса
 	last_error      = (exc_type, exc_value) последняя оштбка доступа к БД
@@ -25,25 +25,31 @@ class dsqlite:
 			self.curs = self.conn.cursor()
 	#	except:
 
-	def	execute (self, query):
+	def	execute (self, query, uvars = None):
 		try:
-			self.curs.execute (query)
+			if uvars:
+				self.curs.execute (query, uvars)
+			else:	self.curs.execute (query)
 			self.last_error = None
 			return	True
 		except (sqlite3.OperationalError, sqlite3.IntegrityError, sqlite3.Warning):
 			self.last_error = sys.exc_info()[:2]
 			return	False
+		finally:
+			self.conn.commit()
 
-	def	get_row (self, query):
+	def	get_row (self, query, uvars = None):
 		return  self.get (query, 0)
 
-	def	get_rows (self, query):
+	def	get_rows (self, query, uvars = None):
 		return  self.get (query, 1)
 
-	def     get (self, query, fall):
+	def     get (self, query, fall, uvars = None):
 		self.last_error = None
 		try:
-			self.curs.execute (query)
+			if uvars:
+				self.curs.execute (query, uvars)
+			else:	self.curs.execute (query)
 			self.desc = [f[0] for f in self.curs.description]
 			if fall:	return  self.curs.fetchall()
 			else:		return  self.curs.fetchone()
@@ -52,6 +58,9 @@ class dsqlite:
 			self.last_error = sys.exc_info()[:2]
 		finally:
 			self.conn.commit()
+
+	def	close():
+		self.conn.close()
 
 	def	get_table (self, tname, swhere = None, cols = None):
 		""" Читать таблицу из БД "SELECT {*|<cols>} FROM <tname> [WHERE <swhere>];"	"""
@@ -62,17 +71,38 @@ class dsqlite:
 		self.rows = self.get_rows (query)
 		if self.rows:	return	self.desc, self.rows
 
+'''
+# Объединяем запросы к базе
+cursor.executescript(""" insert into Artist values (Null, 'A Aagrh!'); insert into Artist values (Null, 'A Aagrh-2!'); """)
+
+# C подставновкой по порядку на места знаков вопросов:
+cursor.execute("SELECT Name FROM Artist ORDER BY Name LIMIT ?", ('2'))
+
+# И с использованием именнованных замен:
+cursor.execute("SELECT Name from Artist ORDER BY Name LIMIT :limit", {"limit": 3})
+
+new_artists = [ ('A Aagrh!',), ('A Aagrh!-2',), ('A Aagrh!-3',), ]
+cursor.executemany("insert into Artist values (Null, ?);", new_artists)
+'''
+
 if __name__ == '__main__':
-	sqls =	["DROP TABLE tst;",
-	"CREATE TABLE tst (id integer PRIMARY KEY, name text);",
-	"INSERT INTO tst (id, name) VALUES (1, 'QWE asdfg');",
-	"INSERT INTO tst (id, name) VALUES (2, 'Привет ЙЦУКЕН');",
-	"INSERT INTO tst (id, name) VALUES (3, 'POIUYTREWQ');",
+	sqls = ["""CREATE TABLE whosts (
+	id_wh INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	host_name TEXT NOT NULL
+	)""",
+	"INSERT INTO whosts (host_name) VALUES ('wialon.rnc52.ru')",
+	"INSERT INTO whosts (host_name) VALUES ('pp-wialon.rnc52.ru')",
+	"INSERT INTO whosts (host_name) VALUES ('sh-wialon.rnc52.ru')",
+	"INSERT INTO whosts (host_name) VALUES ('smp-wialon.rnc52.ru')",
+	"INSERT INTO whosts (host_name) VALUES ('test-wialon.rnc52.ru')",
+	'CREATE TABLE whusers (\n id_whu INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,\n login TEXT NOT NULL,\n passwd TEXT,\n token TEXT,\n token_create INTEGER\n)',
+	"INSERT INTO whusers (login, token) VALUES ('wialon', '1d5a4a6ab2bde440204e6bd1d53b3af82FD7F6B064E042FBBCC978E2B37A2A95930F80E6')",
+	"INSERT INTO whusers (login, token) VALUES ('V.Smirnov', 'c5a76d06f77af04aa4c9fa0699d465c299B67214D257083C5E790742520C44F9EA0E3D80')",
 	]
-	lite = dsqlite('ZZZ.db')
+	lite = dsqlite()	#'wialon.db')
+	print lite.execute("INSERT INTO whosts (host_name) VALUES (?)", ('ZZZZZ',)), lite.last_error
 	for sql in sqls:
 		print sql, lite.execute(sql), lite.last_error
-	print 'SQLite version:', lite.execute('SELECT SQLITE_VERSION()')
-	print 'get_row:', lite.get_row('SELECT SQLITE_VERSION()')
-	print 'get_rows:', lite.get_rows('SELECT * FROM tst WHERE id < 0'), lite.last_error, lite.desc
-	print 'get_table:', lite.get_table ('tst'), lite.last_error
+	print 'SQLite version:', lite.get_row('SELECT SQLITE_VERSION()')
+	print 'get_rows:', lite.get_rows('SELECT * FROM whosts WHERE id_wh > 0'), lite.last_error, lite.desc
+	print 'get_table:', lite.get_table ('whusers'), lite.last_error
